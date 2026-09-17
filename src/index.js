16315c64ddb8957394cb16ccd5c1211c154798f3
gabrielhenrique-pixel <gabriel.henrique@casadocroissant.com.br>
2026-09-17 11:44:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -654,7 +654,7 @@ if (url.pathname === '/api/rentabilidade/clientes' && request.method === 'GET') 
         'NVL(PAR.DESCFIN, 0) AS DESCONTO_FINANCEIRO,',
         'NVL(PAR.AD_COMVENDA, 0) AS COMISSAO_VENDA',
         'FROM TGFPAR PAR',
-        "WHERE PAR.CLIENTE = 'S'",
+        "WHERE PAR.AD_TIPODECLIENTE = 'Cliente'",
         "AND PAR.ATIVO = 'S'",
         'ORDER BY PAR.NOMEPARC'
       ].join(String.fromCharCode(10)));
@@ -704,7 +704,7 @@ if (
       'NVL(PAR.DESCFIN, 0) AS DESCONTO_FINANCEIRO,',
       'NVL(PAR.AD_COMVENDA, 0) AS COMISSAO_VENDA',
       'FROM TGFPAR PAR',
-      "WHERE PAR.CLIENTE = 'S'",
+      "WHERE PAR.AD_TIPODECLIENTE = 'Cliente'",
       "AND PAR.ATIVO = 'S'",
       'ORDER BY PAR.NOMEPARC'
     ].join(String.fromCharCode(10))
