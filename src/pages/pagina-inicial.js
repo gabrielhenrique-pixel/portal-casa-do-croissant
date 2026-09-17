4969c744499f010ec2282df98132b5492ed346dd
gabrielhenrique-pixel <gabriel.henrique@casadocroissant.com.br>
2026-09-17 13:43:14 -0300

    Create pagina-inicial.js

---
+++ src/pages/pagina-inicial.js
@@ -0,0 +1,9 @@
+export function paginaInicialPage() {
+  return `
+    <section id="viewInicio" class="visao">
+      <div class="portal-desenvolvimento">
+        <h2>Portal em desenvolvimento</h2>
+      </div>
+    </section>
+  `;
+}
