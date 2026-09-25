af5da069423586bba5661c98a00733f7956cfe8a
gabrielhenrique-pixel <gabriel.henrique@casadocroissant.com.br>
2026-09-25 15:45:35 -0300

    Update bsc-comercial.js

--- src/pages/bsc-comercial.js
+++ src/pages/bsc-comercial.js
@@ -553,10 +553,7 @@ var registros = abaAtual === 'financeiro-vendedor'
       var registrosVisiveis = registros.filter(function(registro) {
   if (!modoVendedor) return true;
 
-  return (
-    Number(registro.quantidadeAnterior || 0) !== 0 ||
-    Number(registro.quantidadeAtual || 0) !== 0
-  );
+  return Number(devolucao.devolucaoAtual || 0) !== 0;
 });
 
 var devolucoes = Array.isArray(dadosDevolucoes.devolucoes)
